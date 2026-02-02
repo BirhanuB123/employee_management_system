578beef8696b7d3da94546f6f24ccce7b4b7f83c
BirhanuB123 <birhanubayne@gmail.com>
2026-02-02 17:25:23 +0800

    second commit

---
+++ server/test-db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+console.log("Attempting to connect to:", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('SUCCESS: Connected to MongoDB');
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error('ERROR: Could not connect to MongoDB');
+        console.error(err);
+        process.exit(1);
+    });
